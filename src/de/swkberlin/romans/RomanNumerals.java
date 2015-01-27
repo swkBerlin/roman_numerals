f85bcfdb71b46ebb0e197bbe38e6bd84804e5941
Holger Schmeisky <google@schmeisky.com>
2015-01-27 20:51:25 +0100

    subtractive cases (IV, IX) with table entries

--- src/de/swkberlin/romans/RomanNumerals.java
+++ src/de/swkberlin/romans/RomanNumerals.java
@@ -5,16 +5,23 @@ import java.util.Map;
 
 public class RomanNumerals {
 
-    static Map<Integer, String> numerals = new LinkedHashMap() {{
-        put(1000, "M");
-        put(500, "D");
-        put(100, "C");
-        put(50, "L");
-        put(10, "X");
-        put(5, "V");
-        put(1, "I");
-    }};
-
+    static Map<Integer, String> numerals = new LinkedHashMap() {
+        {
+            put(1000, "M");
+            put(900, "CM");
+            put(500, "D");
+            put(400, "CD");
+            put(100, "C");
+            put(90, "XC");
+            put(50, "L");
+            put(40, "XL");
+            put(10, "X");
+            put(9, "IX");
+            put(5, "V");
+            put(4, "IV");
+            put(1, "I");
+        }
+    };
 
     static String result = "";
 
@@ -25,26 +32,7 @@ public class RomanNumerals {
                 i -= entry.getKey();
                 result += entry.getValue();
             }
-
-            for (Map.Entry<Integer, String> subtractor : numerals.entrySet()) {
-                if (subtractor.getKey().toString().startsWith("5"))
-                    continue;
-                i = minusSubtractor(i, entry, subtractor);
-            }
-
         }
-
         return result;
-    }
-
-    private static int minusSubtractor(int i, Map.Entry<Integer, String> entry,
-                                       Map.Entry<Integer, String> subtractor) {
-        if (entry.getKey() > subtractor.getKey()) {
-            if (i == entry.getKey() - subtractor.getKey()) {
-                result += subtractor.getValue() + entry.getValue();
-                i -= entry.getKey();
-            }
-        }
-        return i;
     }
 }
